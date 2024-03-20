6b5e83436fdbfe318a53b4fb00f37dc9e29234f6
Cheehan <pockethook@npyu.org>
2024-03-19 17:39:00 -0700

    Add payload VESC targets

---
+++ hwconf/trampa/vesc6/hw_60_mk6_max_payload.h
@@ -0,0 +1,14 @@
+#ifndef HW_60_MK6_MAX_PAYLOAD_H_
+#define HW_60_MK6_MAX_PAYLOAD_H_
+
+// Limits
+#ifndef MCCONF_L_IN_CURRENT_MAX
+#define MCCONF_L_IN_CURRENT_MAX			16.0 // Input current limit in Amperes (Upper)
+#endif
+#ifndef MCCONF_L_IN_CURRENT_MIN
+#define MCCONF_L_IN_CURRENT_MIN			-12.0 // Input current limit in Amperes (Lower)
+#endif
+
+#include "hw_60_mk6_max_jetty.h"
+
+#endif /* HW_60_MK6_MAX_PAYLOAD_H_ */
